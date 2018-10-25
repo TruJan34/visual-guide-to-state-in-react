6592d3d917603a1ce6582fb7408a024ec7f3c46d
Trushart Jangade <trushartj@kpit.com>
2018-10-25 20:34:37 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,49 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ReactDOM from 'react-dom';
 
 class App extends Component {
+  // 2 constructor is called
+  constructor(props) {
+    super(props);
+
+    // Must initialize state first
+    this.state = { count: 0 };
+  }
+
+  // 6 click handler is called
+  // 8 setState() called
+  handleClick() {
+    // Increment the count when the button is clicked
+    this.setState({
+      count: this.state.count + 1
+    }, function() {
+      // setState is asynchronous! This function gets called
+      // when it's finished.
+      console.log("Job's done");
+    });
+  }
+
+  // 3 render is called
+  // 7 later render is called
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="app">
+        <div className="click-count">
+          Button presses: {this.state.count}
+        </div>
+        <button onClick={this.handleClick.bind(this)}>
+          Add One
+        </button>
       </div>
     );
   }
 }
+
+// 1 intiaial page load
+ReactDOM.render(
+  <App />,
+  document.getElementById('container')
+);
 
 export default App;
